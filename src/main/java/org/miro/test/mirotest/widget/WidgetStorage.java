2822c2701f43e3ba00e89cece819264fa137f3e8
Petrichenko Maksim <mpetrichenko@pminst.ru>
2020-07-28 02:43:22 +0500

    reworked thread safety

--- src/main/java/org/miro/test/mirotest/widget/WidgetStorage.java
+++ src/main/java/org/miro/test/mirotest/widget/WidgetStorage.java
@@ -5,90 +5,120 @@ import org.miro.test.mirotest.LayerHelper;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class WidgetStorage {
 
     private ConcurrentHashMap<Long, Widget> widgetMap = new ConcurrentHashMap<>();
     private LayerHelper layerHelper = new LayerHelper();
     private final AtomicLong idCounter = new AtomicLong(1);
-    private final Object mapLocker = new Object();
+    ReadWriteLock lock = new ReentrantReadWriteLock();
 
     public Iterable<Widget> getAll() {
-        ArrayList<Widget> sortedWidgets = new ArrayList<>(widgetMap.values());
-        sortedWidgets.sort(Comparator.comparingInt(Widget::getZ));
-        return sortedWidgets;
+        lock.readLock().lock();
+        try {
+            ArrayList<Widget> sortedWidgets = new ArrayList<>(widgetMap.values());
+            sortedWidgets.sort(Comparator.comparingInt(Widget::getZ));
+            return sortedWidgets;
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public boolean existZIndex(Integer zIndex) {
-        return widgetMap.values().stream().anyMatch(w -> w.getZ().equals(zIndex));
+        lock.readLock().lock();
+        try {
+            return widgetMap.values().stream().anyMatch(w -> w.getZ().equals(zIndex));
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public long add(Widget widget) {
-        Long id = widget.getId();
-        if (id == null) {
-            id = idCounter.getAndIncrement();
-        }
-        if (widget.getZ() == null) {
-            ArrayList<Widget> sortedWidgets = new ArrayList<>(widgetMap.values());
-            sortedWidgets.sort(Comparator.comparingInt(Widget::getZ).reversed());
-            int nextZIndex = sortedWidgets.get(0).getZ() + 1;
-            widget.setZ(nextZIndex);
-        } else if (existZIndex(widget.getZ())) {
-            synchronized (mapLocker) {
+        lock.writeLock().lock();
+        try {
+            Long id = widget.getId();
+            if (id == null) {
+                id = idCounter.getAndIncrement();
+            }
+            if (widget.getZ() == null) {
+                ArrayList<Widget> sortedWidgets = new ArrayList<>(widgetMap.values());
+                sortedWidgets.sort(Comparator.comparingInt(Widget::getZ).reversed());
+                int nextZIndex = sortedWidgets.get(0).getZ() + 1;
+                widget.setZ(nextZIndex);
+            } else if (existZIndex(widget.getZ())) {
                 layerHelper.moveWidgets(widget.getZ(), getAll());
             }
+            widget.setId(id);
+            widget.setLastModified();
+            widgetMap.put(id, widget);
+            return id;
+        } finally {
+            lock.writeLock().unlock();
         }
-        widget.setId(id);
-        widget.setLastModified();
-        widgetMap.put(id, widget);
-        return id;
     }
 
     public boolean save(long id, Widget widget) {
-        Widget existingWidget = widgetMap.get(id);
-        if (existingWidget == null ) {
-            return false;
-        }
-        widget.setId(id);
-        if (widget.getX() == null) {
-            widget.setX(existingWidget.getX());
-        }
-        if (widget.getY() == null) {
-            widget.setY(existingWidget.getY());
-        }
-        if (widget.getZ() == null) {
-            widget.setZ(existingWidget.getZ());
-        }
-        if (widget.getWidth() == null) {
-            widget.setWidth(existingWidget.getWidth());
-        }
-        if (widget.getHeight() == null) {
-            widget.setHeight(existingWidget.getHeight());
-        }
-        widget.setLastModified();
-        widgetMap.replace(id, widget);
-
-        if (existZIndex(widget.getZ()) && !widget.getZ().equals(existingWidget.getZ())) {
-            synchronized (mapLocker) {
+        lock.writeLock().lock();
+        try {
+            Widget existingWidget = widgetMap.get(id);
+            if (existingWidget == null) {
+                return false;
+            }
+            if (existZIndex(widget.getZ()) && !widget.getZ().equals(existingWidget.getZ())) {
                 layerHelper.moveWidgets(widget.getZ(), getAll());
             }
+            if (widget.getX() != null) {
+                existingWidget.setX(widget.getX());
+            }
+            if (widget.getY() != null) {
+                existingWidget.setY(widget.getY());
+            }
+            if (widget.getZ() != null) {
+                existingWidget.setZ(widget.getZ());
+            }
+            if (widget.getWidth() != null) {
+                existingWidget.setWidth(widget.getWidth());
+            }
+            if (widget.getHeight() != null) {
+                existingWidget.setHeight(widget.getHeight());
+            }
+            existingWidget.setLastModified();
+            return true;
+        } finally {
+            lock.writeLock().unlock();
         }
-        return true;
     }
 
     public Widget getById(long id) {
-        return widgetMap.get(id);
+        lock.readLock().lock();
+        try {
+            return widgetMap.get(id);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public boolean deleteById(long id) {
-        if (!widgetMap.containsKey(id)) {
-            return false;
+        lock.writeLock().lock();
+        try {
+            if (!widgetMap.containsKey(id)) {
+                return false;
+            }
+            widgetMap.remove(id);
+            return true;
+        } finally {
+            lock.writeLock().unlock();
         }
-        widgetMap.remove(id);
-        return true;
     }
 
-    public int size() {
-        return widgetMap.size();
+    public long size() {
+        lock.readLock().lock();
+        try {
+            return widgetMap.mappingCount();
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 }
